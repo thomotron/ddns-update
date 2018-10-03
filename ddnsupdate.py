4396e4331c42f99a70aad8b458875ccea32b2a4c
Thomas Wade <gigatech55@gmail.com>
2018-10-03 18:40:28 +0930

    Initial commit
    Yeet those problems away

---
+++ ddnsupdate.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import requests
+from argparse import ArgumentParser
+from configparser import ConfigParser
+
+parser = ArgumentParser()
+parser.add_argument('-c', '--config', help='path to the config file')
+parser.parse_args()
+
+try:
+    config_path = parser.config
+except:
+    config_path = 'config.ini'
+
+config = ConfigParser()
+config.read(config_path)
+
+if not config.sections():
+    print('No existing config was found')
+    print('Copy the following blank template into ' + config_path + ' and fill in the blanks:')
+    print('[Credentials]\n' + \
+          'host = \n' + \
+          'domain = \n' + \
+          'password = ')
+    exit(1)
+
+if not 'Credentials' in config:
+    print('Failed to read config: \'Credentials\' section missing')
+    exit(1)
+
+if not 'host' in config['Credentials']:
+    print('Failed to read config: \'host\' missing')
+    exit(1)
+
+if not 'domain' in config['Credentials']:
+    print('Failed to read config: \'domain\' missing')
+    exit(1)
+
+if not 'password' in config['Credentials']:
+    print('Failed to read config: \'password\' missing')
+    exit(1)
+
+host = config['Credentials']['host']
+domain = config['Credentials']['domain']
+password = config['Credentials']['password']
+
+p = {
+    "host": host,
+    "domain": domain,
+    "password": password,
+    "ip": "" # Leave blank to use current IP
+}
+
+r = requests.get("https://dynamicdns.park-your-domain.com/update", params=p)
+print(r.status_code)
